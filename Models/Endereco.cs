a142c40d7d4b5a7c043b677ab2126265b370a9f8
IATec - Filipe Nogueira <filipe.nogueira@iatec.com>
2022-03-22 12:05:46 -0300

    Finish

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,31 @@ namespace AspMvcBasic.Models
     public class Endereco : Entity
     {
         public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(200, ErrorMessage = "O Campo {0} Precisa ter Entre {2} e {1} Caracteres.", MinimumLength = 2)]
         public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(50, ErrorMessage = "O Campo {0} Precisa ter Entre {2} e {1} Caracteres.", MinimumLength = 1)]
         public string Numero { get; set; }
+
         public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(8, ErrorMessage = "O Campo {0} Precisa {1} Caracteres.", MinimumLength = 8)]
         public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(100, ErrorMessage = "O Campo {0} Precisa ter Entre {2} e {1} Caracteres.", MinimumLength = 2)]
         public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(100, ErrorMessage = "O Campo {0} Precisa ter Entre {2} e {1} Caracteres.", MinimumLength = 2)]
         public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(50, ErrorMessage = "O Campo {0} Precisa ter Entre {2} e {1} Caracteres.", MinimumLength = 2)]
         public string Estado { get; set; }
 
         /* EF Relation */
--- Models/Fornecedor.cs
+++ Models/Fornecedor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,19 @@ namespace AspMvcBasic.Models
 {
     public class Fornecedor : Entity
     {
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(100, ErrorMessage = "O Campo {0} Precisa ter Entre {2} e {1} Caracteres.", MinimumLength = 2)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(14, ErrorMessage = "O Campo {0} Precisa ter Entre {2} e {1} Caracteres.", MinimumLength = 11)]
         public string Documento { get; set; }
+
         public TipoFornecedor TipoFornecedor  { get; set; }
+
         public Endereco Endereco { get; set; }
+
+        [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
 
         /* EF Relations */
